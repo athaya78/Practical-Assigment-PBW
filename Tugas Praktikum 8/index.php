851fb86f3c5c4b1204f003b7ff10534fceb98d42
Hanna Athaya <98350284+athaya78@users.noreply.github.com>
2022-04-22 23:23:32 +0700

    Add files via upload
    upload tugas 8 praktikum PBW

---
+++ Tugas Praktikum 8/index.php
@@ -0,0 +1,34 @@
+<?php
+
+class Book{
+    private $code_book;
+    private $name;
+    private $qty;
+
+    private function setCodeQty($code_book, $name, $qty){
+        $this->code_book = $code_book;
+        $this->name = $name;
+        $this->qty = $qty;
+    }
+
+    public function __construct($code_book, $name, $qty)
+    {
+        $this->setCodeQty($code_book, $name, $qty);
+    }
+
+    public function getCodeQty(){
+        if(preg_match("/^[A-Z]{2}[0-9]{2}$/", $this->code_book) ){
+            if($this->qty < 1){
+            return "ERROR..! Format salah.";
+            }
+            return $this->code_book . $this->name . $this->qty;
+        }else{
+            return "ERROR..! Format tidak sesuai.";
+        }   
+    }
+}
+
+$book = new Book('BB01', 'Onepiece', 130);
+echo $book->getCodeQty();
+
+?>
